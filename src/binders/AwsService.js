972fb7d4536c35e4f5c1be67b75c742df7b7af7c
Krishna Sameer Achanta <achanta3215@gmail.com>
2024-09-13 22:34:51 +0530

    Allow to configure the S3 endpoint

--- src/binders/AwsService.js
+++ src/binders/AwsService.js
@@ -49,6 +49,7 @@ class AwsService extends CloudService {
           accessKeyId: process.env.AWS_ACCESS_KEY_ID,
           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         },
+        endpoint: process.env.AWS_S3_ENDPOINT,
         region: process.env.AWS_REGION,
       });
     }
